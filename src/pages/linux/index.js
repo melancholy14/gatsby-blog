43ac5551cb618492340f5251b230a09d8db4b3e8
melancholy14 <melancholy8914@gmail.com>
2018-12-18 21:59:13 +0000

    Create index.js

---
+++ src/pages/linux/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../../components/layout';
+
+const LinuxIndexPage = () => (
+  <Layout>
+    <h1>Here is linux pages</h1>
+  </Layout>
+)
+
+export default LinuxIndexPage;
