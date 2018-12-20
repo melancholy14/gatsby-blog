36a61ca798641adf15202447055dde5ab74cb794
melancholy14 <melancholy8914@gmail.com>
2018-12-20 23:10:40 +0000

    Add new page: linux/nslookup-options

--- src/pages/linux/index.js
+++ src/pages/linux/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../../components/layout';
 
 const LinuxIndexPage = () => (
   <Layout>
     <h1>Here is linux pages</h1>
+    <Link to="linux/nslookup-options">nslookup options</Link>
   </Layout>
 )
 
---
+++ src/pages/linux/nslookup-options.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '../../components/layout';
+
+const NslookupOptionsPage = () => (
+  <Layout>
+    <div className="date">July 8, 2018</div>
+    <div className="title">nslookup options</div>
+    <div>
+      Simply put, <span>nslookup</span> shorten to Name Servers Lookup.
+      This is one of network managing tools on Linux. You can query for finding a domain name and an ip address.
+      For more info, please check <a href="https://en.wikipedia.org/wiki/nslookup">https://en.wikipedia.org/wiki/nslookup</a>
+    </div>
+  </Layout>
+)
+
+export default NslookupOptionsPage;
